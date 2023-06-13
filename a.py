29d556a4d7b6d578c9244018f4c2a830f10e3979
Rafael Tsutomu <127237352+rafaelTsu@users.noreply.github.com>
2023-06-13 17:17:42 -0400

    Create a.py

---
+++ a.py
@@ -0,0 +1,12 @@
+1
+lista = list(map(int, input().split()))
+numinserido, posicao = map(int, input().split())
+
+lista.insert(posicao, numinserido)
+print(lista)
+2
+lista = list(map(int, input().split()))
+posicao = int(input())
+
+print(lista[posicao])
+lista.pop(po
